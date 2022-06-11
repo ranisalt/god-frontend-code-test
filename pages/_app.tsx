41f5985a6f4bfe948048004884323de4652bf8e8
Ranieri Althoff <ranisalt@gmail.com>
2022-06-11 18:25:09 +0200

    Add design system component library & split layout from page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
-import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
 import React from "react";
+import { StyleProvider, ThemePicker } from "vcc-ui";
 
-function HomePage() {
+type AppPropsWithLayout = AppProps & {
+  Component: NextPage;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <React.StrictMode>
-      <HelloWorld />
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <Component {...pageProps} />
+        </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
   );
-}
+};
 
-export default HomePage;
+export default App;
