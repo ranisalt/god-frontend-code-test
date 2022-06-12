7ac094957cc72428524e027e24f7e0c99a0785e8
Ranieri Althoff <ranisalt@gmail.com>
2022-06-12 19:09:10 +0200

    Add mobile circle buttons to jump on the carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import CARS from "../public/api/cars.json";
 import Image from "next/image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Block, Flex, IconButton, Link, Text, useTheme, View } from "vcc-ui";
 
 type Car = typeof CARS[0];
@@ -10,26 +10,43 @@ const isDivElement = (el: ChildNode | null): el is HTMLDivElement =>
 
 type ScrollDirection = "forward" | "back";
 
+const getChildWidth = (current: HTMLDivElement) => {
+  const { firstChild } = current;
+  if (!isDivElement(firstChild)) return;
+
+  const { nextSibling } = firstChild;
+  if (!isDivElement(nextSibling)) return;
+
+  return nextSibling.offsetLeft - firstChild.offsetLeft;
+};
+
 const HomePage = () => {
-  const { tokens, ...theme } = useTheme();
+  const theme = useTheme();
+  const [currentIndex, setCurrentIndex] = useState(0);
   const gridRef = useRef<HTMLDivElement>(null);
 
   const scroll = (direction: ScrollDirection) => {
     const { current } = gridRef;
     if (!current) return;
 
-    const { firstChild } = current;
-    if (!isDivElement(firstChild)) return;
+    const columnWidth = getChildWidth(current);
+    if (!columnWidth) return;
 
-    const { nextSibling } = firstChild;
-    if (!isDivElement(nextSibling)) return;
-
-    const amount = nextSibling.offsetLeft - firstChild.offsetLeft;
     current.scrollBy({
-      left: direction === "forward" ? amount : -amount,
+      left: direction === "forward" ? columnWidth : -columnWidth,
       behavior: "smooth",
     });
   };
+
+  useEffect(() => {
+    const { current } = gridRef;
+    if (!current) return;
+
+    const columnWidth = getChildWidth(current);
+    if (!columnWidth) return;
+
+    current.scrollTo({ left: currentIndex * columnWidth, behavior: "smooth" });
+  }, [currentIndex]);
 
   return (
     <>
@@ -56,7 +73,10 @@ const HomePage = () => {
         paddingTop={0}
         direction="row"
         justifyContent="end"
-        extend={{ columnGap: theme.baselineGrid }}
+        extend={{
+          columnGap: theme.baselineGrid,
+          [theme.breakpoints.untilM]: { display: "none" },
+        }}
       >
         {/* these buttons should use mediacircled-previous and mediacircled-next, but they are missing from possible values of "iconName" */}
         <IconButton
@@ -72,25 +92,58 @@ const HomePage = () => {
           variant="outline"
         />
       </View>
+
+      <View
+        padding={4}
+        paddingTop={0}
+        direction="row"
+        justifyContent="center"
+        extend={{
+          columnGap: theme.baselineGrid / 2,
+          [theme.breakpoints.fromM]: { display: "none" },
+        }}
+      >
+        {CARS.map((_, index) => (
+          <Block
+            key={index}
+            extend={{ cursor: "pointer", padding: theme.baselineGrid / 2 }}
+            onClick={() => setCurrentIndex(index)}
+          >
+            <Block
+              extend={{
+                backgroundColor:
+                  currentIndex === index
+                    ? theme.color.foreground.primary
+                    : theme.color.ornament.divider,
+                borderRadius: "50%",
+                height: theme.baselineGrid,
+                // there is an attribute "animation" on the theme object, but it is not typed. 150ms is equivalent to the "elk" duration
+                transition: "150ms ease-in-out",
+                width: theme.baselineGrid,
+              }}
+            />
+          </Block>
+        ))}
+      </View>
     </>
   );
 };
 
 const ListEntry = ({ id, bodyType, imageUrl, modelName, modelType }: Car) => {
-  const { breakpoints, color, tokens, ...theme } = useTheme();
+  const theme = useTheme();
 
   return (
     <Block
       extend={{
         scrollSnapAlign: "center",
-        [breakpoints.fromM]: { scrollSnapAlign: "start" },
+        [theme.breakpoints.fromM]: { scrollSnapAlign: "start" },
       }}
     >
       <Text
         variant="bates"
         subStyle="emphasis"
         extend={{
-          color: color.foreground.secondary,
+          color: theme.color.foreground.secondary,
           textTransform: "uppercase",
         }}
       >
@@ -101,7 +154,7 @@ const ListEntry = ({ id, bodyType, imageUrl, modelName, modelType }: Car) => {
         extend={{
           columnGap: theme.baselineSubGrid,
           marginBottom: 2 * theme.baselineGrid,
-          [breakpoints.fromM]: { flexDirection: "row" },
+          [theme.breakpoints.fromM]: { flexDirection: "row" },
         }}
       >
         <Text as="span" variant="columbus" subStyle="emphasis">
@@ -111,7 +164,7 @@ const ListEntry = ({ id, bodyType, imageUrl, modelName, modelType }: Car) => {
         <Text
           as="span"
           variant="columbus"
-          extend={{ color: color.foreground.secondary }}
+          extend={{ color: theme.color.foreground.secondary }}
         >
           {modelType}
         </Text>
@@ -120,9 +173,11 @@ const ListEntry = ({ id, bodyType, imageUrl, modelName, modelType }: Car) => {
       <Block
         extend={{
           aspectRatio: "4 / 3",
-          width: 290,
-          position: "relative",
           marginBlock: theme.baselineGrid,
+          position: "relative",
+          // each item is as wide as possible, sans the container padding (4 * baseline grid on each side), so it sits perfectly centered
+          width: `calc(100vw - ${8 * theme.baselineGrid}px)`,
+          [theme.breakpoints.fromM]: { width: 290 },
         }}
       >
         <Image
@@ -136,7 +191,7 @@ const ListEntry = ({ id, bodyType, imageUrl, modelName, modelType }: Car) => {
 
       <Flex
         extend={{
-          columnGap: tokens.buttonPaddingHorizontal,
+          columnGap: theme.tokens.buttonPaddingHorizontal,
           flexDirection: "row",
           justifyContent: "center",
         }}
